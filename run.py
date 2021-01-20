6afcfc7c18c4e95ed1981c8c8cdc4600ee2bfd73
Loic Alain Royer <royerloic@gmail.com>
2021-01-19 19:43:04 -0800

    adapted script for team

---
+++ run.py
@@ -0,0 +1,31 @@
+
+from exporter import ScholarExporter
+
+# Loic Royer:
+s = ScholarExporter.from_user('upcbWQQAAAAJ')  # Loic A Royer user
+s.export('loicroyer.html')
+
+# Merlin Lange:
+s = ScholarExporter.from_user('CFKSx6MAAAAJ')
+s.export('merlinlange.html')
+
+# Bin Yang:
+s = ScholarExporter.from_user('L3pLCwQAAAAJ')
+s.export('binyang.html')
+
+# Hirofumi Kobayashi:
+s = ScholarExporter.from_user('C8IK_JAAAAAJ')
+s.export('hirofumikobayashi.html')
+
+# Ahmet Can Solak:
+s = ScholarExporter.from_user('7dILcKsAAAAJ')
+s.export('ahmetcansolak.html')
+
+# Shruthi Vijay Kumar:
+s = ScholarExporter.from_user('NV6MuzkAAAAJ')
+s.export('shruthivijaykumar.html')
+#
+# # Luke Hyman:
+# s = ScholarExporter.from_user('?')  # Loic A Royer user
+# s.export('lukehyman.html')
+
